5adf75df9cbf47736a1cb41855abdb4d7a5128c4
cesartidrick <cesartidrick@gmail.com>
2022-04-05 10:45:17 -0600

    commit tree f181db3fe78a0a17cad1f3853abf525b14415a06

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let say = '';
+
+let feet;
+
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    say = 'This one is on me!';
+  } else if (feet >= 400 && feet <= 2000) {
+    say = 'That will be twenty bucks.';
+  } else if (feet >= 2000 && feet <= 2500) {
+    say = 'I will gladly take your thirty bucks.';
+  } else if (feet >= 2500) {
+    say = 'No can do.';
+  }
+
+  return say;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+let city = '';
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  city === 'NYC' ? say = 'Ok, sounds good.' : say = 'No go.';
+  return say;
+};
+
+let tip;
+
+let phrase;
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      phrase = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      phrase = 'Thank you.';
+      break;
+    default:
+      phrase = 'Bye.';
+      break;
+
+  };
+  return(phrase);
 }
